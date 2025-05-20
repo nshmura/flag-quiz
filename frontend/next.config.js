4b72e36a1c15a8f4dd355059dfaac9f662ec9b41
Shinichi Nishimura <shinichi.nishimura@japan-d2.com>
2025-05-20 18:44:18 +0900

    Add deploy.yml

---
+++ frontend/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/flag-quiz',
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig; 
