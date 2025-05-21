cc5352441f1d5b8d4cc46bbc8dcb5036897f181d
nshmura <nshmura.s@gmail.com>
2025-05-21 18:46:50 +0900

    fix next.config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const SUB_DIRECTORY = "/flag-quiz"; //これは適切な値にしてください。
+
+const isProd = process.env.NODE_ENV == "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: isProd ? SUB_DIRECTORY : "",
+  assetPrefix: isProd ? SUB_DIRECTORY : "",
+  publicRuntimeConfig: {
+    basePath: isProd ? SUB_DIRECTORY : "",
+  },
 };
 
 export default nextConfig;
